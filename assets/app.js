a3abf495513b3b8107495401789f516a2fd1bf4f
sebastien <naertsebastien@gmail.com>
2025-10-06 12:03:36 +0200

    Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,14 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+
+// Installer bootstrap avec importmap => php bin/console importmap:require bootstrap
+// ça ajoute bootstrap et @popperjs dans le dossier vendor
+// Si je viens d'un projet github en plus du composer install, faire php bin/console importmap:install
+// Import ./ => je viens chercher quelque chose dans le dossier assets
+// import bootstrap => je viens chercher le fichier bootstrap.js dans le dossier vendor/bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
